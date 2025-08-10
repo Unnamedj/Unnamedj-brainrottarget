63587575666206d0580e9aafcc84fb2dfe1b7696
Unnamedj <zamorajosmar948@gmail.com>
2025-08-10 16:29:39 -0600

    Actualizar brainrot_sniper.lua

--- brainrot_sniper.lua
+++ brainrot_sniper.lua
@@ -8,7 +8,8 @@ getgenv().WebhookATargets = {
     "La Grande Combinasion",
     "Garama and Madundung",
     "Nuclearo Dinossauro",
-    "Los Combinasionas"
+    "Los Combinasionas",
+    "Karkerkar Kurkur"
 }
 
 getgenv().WebhookBTargets = {
